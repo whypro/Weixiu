7697f8c583dc594c3eda6f951d9b6b3d5a110cba
whypro <whypro@live.cn>
2013-06-30 23:49:09 +0800

    修正 config.py，修改 css

--- config.py
+++ config.py
@@ -1,17 +1,7 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
+import os
 
-try:
-    import  bae
-except:
-    # 测试服务器配置
-    # 数据库配置
-    DB_HOST = 'localhost'
-    DB_DATABASE = 'weixiu'
-    DB_USERNAME = 'root'
-    DB_PASSWORD = 'whypro'
-    DB_CHARSET = 'utf8'
-    DB_PORT = 3306
-else:
+if 'SERVER_SOFTWARE' in os.environ:
     # 生产服务器配置
     from bae.core import const
     # 数据库配置
@@ -21,6 +11,15 @@ else:
     DB_PASSWORD = const.MYSQL_PASS
     DB_CHARSET = 'utf8'
     DB_PORT = int(const.MYSQL_PORT)
+else:
+    # 测试服务器配置
+    # 数据库配置
+    DB_HOST = 'localhost'
+    DB_DATABASE = 'weixiu'
+    DB_USERNAME = 'root'
+    DB_PASSWORD = 'whypro'
+    DB_CHARSET = 'utf8'
+    DB_PORT = 3306
 
 #
 DEBUG = True
